0d09f39140180b4a2448435b5072c93a51ea2e85
dillmanl2093 <dillmanl2093@CA-SU-F106-30.sduhsd.lan>
2019-03-06 09:04:39 -0800

    Elevens Starter Code

---
+++ src/Elevens/BlackJackCard.java
@@ -0,0 +1,25 @@
+package Elevens;
+public class BlackJackCard extends Card
+{
+  	//constructors
+  	public BlackJackCard( String s, int f)
+  	{
+            super(s, f);    // this is calling Card constructor
+  	}
+
+  	//accessors
+	public int getValue()
+	{
+            if( getFace() == 1 )    // BlackJack rules, A
+		return 11;
+            if( getFace() >= 10 )   // 10, J, Q, K
+		return 10;
+            return getFace();       // 2 - 9
+	}
+
+  	//toString
+  	public String toString()
+  	{
+            return super.toString() + " " + getValue();
+  	}
+ }
---
+++ src/Elevens/Deck.java
@@ -0,0 +1,34 @@
+package Elevens;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Deck 
+{
+    public static final int NUM_CARDS_DECK = 52;      // constant number of cards per deck
+    public static final int NUM_CARDS_SUIT = 13;      // constant number of cards per suit
+    public static String[] SUITS = "CLUBS HEARTS DIAMONDS SPADES".split(" ");
+
+    private List<Card> cards;
+    private int top;
+    
+    public Deck() {
+        cards = new ArrayList<Card>();          // create an empty card ArrayList
+        top = NUM_CARDS_DECK - 1;               // last card on top by default
+        for (String m: SUITS){
+            for (int i = 1; i <= NUM_CARDS_SUIT; i++){
+                cards.add(new BlackJackCard(m, i));   // actually, add new BlackJackCard
+            }
+        }
+    }
+    
+    public String dealCard(){
+        return cards.get(top--).toString();
+    }
+    
+    public void shuffle(){
+        Collections.shuffle(cards);             // this is from Collections directly
+        top = NUM_CARDS_DECK - 1;               // reset top to the top card
+    }
+}
