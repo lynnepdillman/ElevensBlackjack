3ce12171668aa0275a844e9d3bb3b7c0bcc9b74f
lynnepdillman <45905485+lynnepdillman@users.noreply.github.com>
2019-04-05 19:48:08 -0700

    Update Dealer.java

--- src/ElevensBlackjack/Dealer.java
+++ src/ElevensBlackjack/Dealer.java
@@ -1,4 +1,6 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+/package BlackJack;
+
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -6,33 +8,38 @@
 
 
 //define Dealer class here
-	
-	
-	
-	
+public class Dealer extends AbstractPlayer
+{
 	//instance variable - Deck 
-
-
-
-
+        Deck cards = new Deck();
 
 	//constructors
-
-
-
-
+        public Dealer()
+        {
+            super();
+        }
 
 	//method to shuffle
-
-
-
-
-
+        public void shuffle()
+        {
+            cards.shuffle();
+        }
 
 	//method to deal a card
-
-
-
-
+        public Card deal()
+        {
+            return cards.nextCard();
+        }
 
 	//hit method goes here
+        public boolean hit()
+        {
+            if(getHandValue() < 17)
+            {
+                addCardToHand(deal());
+                return true;
+            }
+            
+            return false;
+        }
+}
