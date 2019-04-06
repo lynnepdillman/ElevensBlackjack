2d69508d57693e4e88b9e3702e7eda70d19c6409
lynnepdillman <45905485+lynnepdillman@users.noreply.github.com>
2019-04-05 19:49:53 -0700

    Update Player.java

--- src/ElevensBlackjack/Player.java
+++ src/ElevensBlackjack/Player.java
@@ -1,4 +1,6 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+package BlackJack;
+
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -10,15 +12,17 @@ import java.util.Scanner;
 
 
 //define Player class here 
+public class Player extends AbstractPlayer
+{
+    //constructors
+    public Player()
+    {
+        super();
+    }
 
-
-
-
-	//constructors
-
-
-
-
-
-	//hit method goes here
-
+    //hit method goes here
+    public boolean hit()
+    {
+        return false;
+    }
+}
